69ad3ee7ccd2043b5fc8e5807416dfab73809123
appleboy <appleboy.tw@gmail.com>
2023-05-13 12:10:56 +0800

    refactor: improve code clarity and expand test coverage
    - Convert key to uppercase in `getGlobalValue` function
    - Add comments to explain checking for "INPUT_<KEY>" environment variable and returning its value
    - Add comments to explain fallback to "<KEY>" environment variable when "INPUT_<KEY>" is not set
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ func main() {
 }
 
 func getGlobalValue(key string) string {
-	key = strings.ToUpper(key)
+	key = strings.ToUpper(key) // Convert key to uppercase
 
+	// Check if there is an environment variable with the format "INPUT_<KEY>"
 	if value := os.Getenv("INPUT_" + key); value != "" {
-		return value
+		return value // Return the value of the "INPUT_<KEY>" environment variable
 	}
 
+	// If the "INPUT_<KEY>" environment variable doesn't exist or is empty,
+	// return the value of the "<KEY>" environment variable
 	return os.Getenv(key)
 }
