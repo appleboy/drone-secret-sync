50f309e94f9b8efafa7f2cd28aa216395c09cd11
appleboy <appleboy.tw@gmail.com>
2023-05-13 12:48:36 +0800

    chore: improve API usage and authentication handling
    - Add drone-go and oauth2 dependencies to go.mod
    - Import "fmt", "github.com/drone/drone-go/drone", and "golang.org/x/oauth2" in main.go
    - Replace os.Getenv("PORT") with drone token and host retrieval
    - Create an HTTP client with OAuth2 authentication
    - Initialize the drone client with the authenticator
    - Retrieve and print the current user's login information
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/appleboy/drone-secrets
 
 go 1.20
+
+require (
+	github.com/drone/drone-go v1.7.1
+	golang.org/x/oauth2 v0.8.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
+
+	"github.com/drone/drone-go/drone"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	os.Getenv("PORT")
+	token := getGlobalValue("drone_token")
+	host := getGlobalValue("drone_server")
+
+	if token == "" {
+		panic("missing drone host")
+	}
+
+	if host == "" {
+		panic("missing drone token")
+	}
+
+	// create an http client with oauth authentication.
+	config := new(oauth2.Config)
+	auther := config.Client(
+		oauth2.NoContext,
+		&oauth2.Token{
+			AccessToken: token,
+		},
+	)
+
+	// create the drone client with authenticator
+	client := drone.NewClient(host, auther)
+
+	// gets the current user
+	user, err := client.Self()
+	if err != nil {
+		panic("get user failed: " + err.Error())
+	}
+	fmt.Printf("login user: %s", user.Login)
 }
 
 func getGlobalValue(key string) string {
